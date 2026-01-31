b1a7b71ba8c99430ce47d5265624448ebb15cf28
Andoitz Campo <acp@adotcpio.org>
2026-01-31 21:21:03 +0100

    add previous exercise tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,31 +1,57 @@
 #include "ScavTrap.hpp"
+#include <iostream>
 
 int main(void)
 {
-    ScavTrap foo;
-    ScavTrap bar("bar");
-
-    for (int i = 0; i < 52; i++)
     {
-        foo.attack("bar");
+        std::cout << "== TEST CLAPTRAP ==\n" << std::endl;
+        ClapTrap foo;
+        ClapTrap bar("bar");
+
+        for (int i = 0; i < 12; i++)
+        {
+            foo.attack("bar");
+        }
+        ClapTrap fooCopy(foo);
+        fooCopy.attack("foo");
+        foo.beRepaired(1000);
+        foo.takeDamage(1000);
+
+        bar.takeDamage(9);
+        bar.beRepaired(2);
+        bar.beRepaired(9);
+        bar.takeDamage(9);
+        bar.takeDamage(1);
+
+        ClapTrap barEqual = bar;
+        barEqual.takeDamage(1);
     }
-    ScavTrap fooCopy(foo);
-    fooCopy.attack("foo");
-    foo.beRepaired(1000);
-    foo.takeDamage(1000);
-
-    bar.takeDamage(9);
-    bar.beRepaired(2);
-    bar.beRepaired(9);
-    bar.takeDamage(9);
-    bar.takeDamage(1);
-
-    ScavTrap barEqual = bar;
-    barEqual.takeDamage(1);
-
-    ScavTrap guard;
-    guard.guardGate();
-    guard.guardGate();
-
+    {
+        std::cout << "\n\n== TEST SCAVTRAP ==\n" << std::endl;
+        ScavTrap foo;
+        ScavTrap bar("bar");
+        
+        for (int i = 0; i < 52; i++)
+        {
+            foo.attack("bar");
+        }
+        ScavTrap fooCopy(foo);
+        fooCopy.attack("foo");
+        foo.beRepaired(1000);
+        foo.takeDamage(1000);
+        
+        bar.takeDamage(9);
+        bar.beRepaired(2);
+        bar.beRepaired(9);
+        bar.takeDamage(9);
+        bar.takeDamage(1);
+        
+        ScavTrap barEqual = bar;
+        barEqual.takeDamage(1);
+        
+        ScavTrap guard;
+        guard.guardGate();
+        guard.guardGate();
+    }
     return 0;
 }
